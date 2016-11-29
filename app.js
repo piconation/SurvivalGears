db70d7c11e68f825a283c6b456e65e888e6eb5b4
mattpowell <piconation@yahoo.com>
2016-11-29 09:23:09 -0700

    2

--- app.js
+++ app.js
@@ -4,23 +4,23 @@
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
 (function(){
-    angular.module('starter', ['ionic', 'firebase'])
+    angular.module('starter', ['ui.router'])
 
-        .run(function ($ionicPlatform) {
-            $ionicPlatform.ready(function () {
-                // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-                // for form inputs)
-                if (window.cordova && window.cordova.plugins.Keyboard) {
-                    cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-                    cordova.plugins.Keyboard.disableScroll(true);
-
-                }
-                if (window.StatusBar) {
-                    // org.apache.cordova.statusbar required
-                    StatusBar.styleDefault();
-                }
-            });
-        })
+        // .run(function ($ionicPlatform) {
+        //     $ionicPlatform.ready(function () {
+        //         // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        //         // for form inputs)
+        //         if (window.cordova && window.cordova.plugins.Keyboard) {
+        //             cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        //             cordova.plugins.Keyboard.disableScroll(true);
+        //
+        //         }
+        //         if (window.StatusBar) {
+        //             // org.apache.cordova.statusbar required
+        //             StatusBar.styleDefault();
+        //         }
+        //     });
+        // })
 
         .config(function ($stateProvider, $urlRouterProvider) {
             $stateProvider
