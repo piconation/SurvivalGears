66675a3a2c5d85a88c8e2c66f59383117ec0309c
mattpowell <piconation@yahoo.com>
2016-12-06 10:12:17 -0700

    commented out mongodb

--- www/js/app.js
+++ www/js/app.js
@@ -74,16 +74,16 @@ angular.module('starter', ['ionic'])
    $urlRouterProvider.otherwise('/login');
 });
 
-var MongoClient = require('mongodb').MongoClient
-    , assert = require('assert');
-
-// Connection URL
-var url = 'mongodb://localhost:27017/myproject';
-
-// Use connect method to connect to the server
-MongoClient.connect(url, function(err, db) {
-    assert.equal(null, err);
-    console.log("Connected successfully to server");
-
-    db.close();
-});
+// var MongoClient = require('mongodb').MongoClient
+//     , assert = require('assert');
+//
+// // Connection URL
+// var url = 'mongodb://localhost:27017/myproject';
+//
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, db) {
+//     assert.equal(null, err);
+//     console.log("Connected successfully to server");
+//
+//     db.close();
+// });
