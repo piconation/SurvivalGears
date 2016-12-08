37fb4581363f2bc118104f34b7d9f0c1a7885b72
mattpowell <piconation@yahoo.com>
2016-12-08 08:24:56 -0700

    change app.get to /, changed render to login.html

--- server.js
+++ server.js
@@ -10,14 +10,14 @@ var passport = require('passport');
     app = express();
 var pg = require('pg');
 
-app.get('/login.html', function (request, response) {
+app.get('/', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('SELECT * FROM test_table', function(err, result) {
             done();
             if (err)
             { console.error(err); response.send("Error " + err); }
             else
-            { response.render('pages/db', {results: result.rows} ); }
+            { response.render('login.html', {results: result.rows} ); }
         });
     });
 });
