9d12bd62e49cefe9d2daa0c3ba3ae59f32098953
mattpowell <piconation@yahoo.com>
2016-12-08 08:17:44 -0700

    change app.get destination to login.html

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var passport = require('passport');
     app = express();
 var pg = require('pg');
 
-app.get('/db', function (request, response) {
+app.get('/login.html', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('SELECT * FROM test_table', function(err, result) {
             done();
