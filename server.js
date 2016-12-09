4a4c5c41e09d2f74bf544073f95bd6050d717dd3
mattpowell <piconation@yahoo.com>
2016-12-09 09:55:24 -0700

    changed GET path back to /www

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use(passport.initialize());
 app.use(passport.session());
 // app.use(app.router); *** DEPRECATED, DO NOT USE ***
 
-app.get('/', function (request, response) {
+app.get('/www', function (request, response) {
     pg.connect(process.env.DATABASE_URL, function(err, client, done) {
         client.query('SELECT * FROM test_table', function(err, result) {
             done();
